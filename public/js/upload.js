f0c1e4e9ec03332ec7ce2bc7a0e09bfaff031ccc
Christopher Grabski <grabc59@gmail.com>
2017-02-08 20:00:49 -0700

    uploads to s3 work, deleting only removes the db record, not the s3 object

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,71 +1,62 @@
 'use strict';
 
-$('.upload-btn').on('click', function (){
-    $('#upload-input').click();
-    $('.progress-bar').text('0%');
-    $('.progress-bar').width('0%');
-});
-
-$('#upload-input').on('change', function(){
-
-  var files = $(this).get(0).files;
-
-  if (files.length > 0){
-    // create a FormData object which will be sent as the data payload in the
-    // AJAX request
-    var formData = new FormData();
-
-    // loop through all the selected files and add them to the formData object
-    for (var i = 0; i < files.length; i++) {
-      var file = files[i];
-
-      // add the files to formData object for the data payload
-      formData.append('uploads[]', file, file.name);
-    }
-
-    $.ajax({
-      url: '/uploads',
-      type: 'POST',
-      data: formData,
-      processData: false,
-      contentType: false,
-      success: function(data){
-          console.log('upload successful!\n' + data);
-      },
-      error: function(data, err) {
-        console.log('upload failed\n'+ JSON.stringify(data) + err);
-      },
-      xhr: function() {
-        // create an XMLHttpRequest
-        var xhr = new XMLHttpRequest();
-
-        // listen to the 'progress' event
-        xhr.upload.addEventListener('progress', function(evt) {
-
-          if (evt.lengthComputable) {
-            // calculate the percentage of upload completed
-            var percentComplete = evt.loaded / evt.total;
-            percentComplete = parseInt(percentComplete * 100);
-
-            // update the Bootstrap progress bar with the new percentage
-            $('.progress-bar').text(percentComplete + '%');
-            $('.progress-bar').width(percentComplete + '%');
-
-            // once the upload reaches 100%, set the progress bar text to done
-            if (percentComplete === 100) {
-              $('.progress-bar').html('Done');
-              window.setTimeout( function(){
-                window.location.href = "/landing";
-              }, 1600);
-              }
-
-            }
-
-        }, false);
-
-        return xhr;
+function uploadFile(file, signedRequest, url){
+  const xhr = new XMLHttpRequest();
+  xhr.open('PUT', signedRequest);
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4){
+      if(xhr.status === 200){
+        window.setTimeout( function(){
+          window.location.href = "/landing";
+        }, 1600);
       }
-    });
+      else{
+        alert('Could not upload file.');
+      }
+    }
+  };
+  xhr.send(file);
+}
 
+/*
+  Function to get the temporary signed request from the app.
+  If request successful, continue to upload the file using this signed
+  request.
+*/
+function getSignedRequest(file){
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `/uploads/sign-s3?file-name=${file.name}&file-type=${file.type}`);
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4){
+      console.log(xhr.responseText);
+      if(xhr.status === 200){
+        const response = JSON.parse(xhr.responseText);
+        uploadFile(file, response.signedRequest, response.url);
+      }
+      else{
+        alert('Could not get signed URL.');
+      }
+    }
+  };
+  xhr.send();
+}
+
+/*
+ Function called when file input updated. If there is a file selected, then
+ start upload procedure by asking for a signed request from the app.
+*/
+function initUpload(){
+  const files = document.getElementById('file-input').files;
+  const file = files[0];
+  if(file == null){
+    return alert('No file selected.');
   }
-});
+  getSignedRequest(file);
+}
+
+/*
+ Bind listeners when the page loads.
+*/
+(() => {
+    document.getElementById('file-input').onchange = initUpload;
+})();
