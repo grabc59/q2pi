e5b8d806695f3a1c7eeca1feeec3aa9bce33cab7
Christopher Grabski <grabc59@gmail.com>
2017-02-09 00:44:19 -0700

    added progress bar back in

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,20 +1,42 @@
-'use strict';
+  'use strict';
 
 function uploadFile(file, signedRequest, url){
+  $('.progress-bar').text('0%');
+  $('.progress-bar').width('0%');
   const xhr = new XMLHttpRequest();
   xhr.open('PUT', signedRequest);
-  xhr.onreadystatechange = () => {
-    if(xhr.readyState === 4){
-      if(xhr.status === 200){
+  // listen to the 'progress' event
+  xhr.upload.addEventListener('progress', function(evt) {
+    if (evt.lengthComputable) {
+      // calculate the percentage of upload completed
+      var percentComplete = evt.loaded / evt.total;
+      percentComplete = parseInt(percentComplete * 100);
+
+      // update the Bootstrap progress bar with the new percentage
+      $('.progress-bar').text(percentComplete + '%');
+      $('.progress-bar').width(percentComplete + '%');
+
+      // once the upload reaches 100%, set the progress bar text to done
+      if (percentComplete === 100) {
+        $('.progress-bar').html('Done, redirecting...');
         window.setTimeout( function(){
           window.location.href = "/landing";
         }, 1600);
       }
-      else{
-        alert('Could not upload file.');
-      }
     }
-  };
+  }, false);
+  // xhr.onreadystatechange = () => {
+  //   if(xhr.readyState === 4){
+  //     if(xhr.status === 200){
+  //       window.setTimeout( function(){
+  //         window.location.href = "/landing";
+  //       }, 1600);
+  //     }
+  //     else{
+  //       alert('Could not upload file.');
+  //     }
+  //   }
+  // };
   xhr.send(file);
 }
 
